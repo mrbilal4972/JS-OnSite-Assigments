f9cc6444bd1aef8672abbe87dde2806456afe277
mrbilal4972 <mr.bilal4972@gmail.com>
2019-12-29 02:04:44 +0500

    JS ch_15.16.17 final commit

--- ch_15,16,17/ch_15,16,17.js
+++ ch_15,16,17/ch_15,16,17.js
@@ -1,27 +1,17 @@
 var courses = [];
-var course1 = prompt("course 1", "Computer");
-var course2 = prompt("course 2", "English");
-var course3 = prompt("course 3", "Urdu");
-var course4 = prompt("course 4", "Physics");
-var course5 = prompt("course 5", "Maths");
 
-courses.push(course1);
-courses.push(course2);
-courses.push(course3);
-courses.push(course4);
-courses.push(course5);
+courses.push(prompt("course 1", "Computer"));
+courses.push(prompt("course 2", "English"));
+courses.push(prompt("course 3", "Urdu"));
+courses.push(prompt("course 4", "Physics"));
+courses.push(prompt("course 5", "Maths"));
 
 alert(courses);
 
-course1 = prompt("course 1", courses[0]);
-courses.splice(0,1,course1);
-course2 = prompt("course 2", courses[1]);
-courses.splice(1,1,course2);
-course3 = prompt("course 3", courses[2]);
-courses.splice(2,1,course3);
-course4 = prompt("course 4", courses[3]);
-courses.splice(3,1,course4);
-course5 = prompt("course 5", courses[4]);
-courses.splice(4,1,course5);
+courses.splice(0,1,prompt("course 1", courses[0]));
+courses.splice(1,1,prompt("course 2", courses[1]));
+courses.splice(2,1,prompt("course 3", courses[2]));
+courses.splice(3,1,prompt("course 4", courses[3]));
+courses.splice(4,1,prompt("course 5", courses[4]));
 
 alert(courses);
