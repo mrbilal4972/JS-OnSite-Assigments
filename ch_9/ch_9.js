e60d1ad7d09659e77ece54452578a7a0f7621e57
mrbilal4972 <mr.bilal4972@gmail.com>
2019-12-28 17:50:56 +0500

    JS ch_9 commit

---
+++ ch_9/ch_9.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Enter your name", "Bilal");
+var lastName = prompt("Enter your last Name", "Ashraf");
+var age = Number(prompt("Enter your age"));
+
+alert("Hello " +firstName+ " " +lastName+", " + "You are " +age+ " years old");
